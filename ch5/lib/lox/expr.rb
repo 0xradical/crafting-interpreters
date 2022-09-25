54c751849458ccc602ce46021da9cc89043db6a6
Thiago Brandão <194487+thiagobrandam@users.noreply.github.com>
2022-09-25 19:06:26 -0300

    Add AST Node Class generator

--- ch5/lib/lox/expr.rb
+++ ch5/lib/lox/expr.rb
@@ -2,13 +2,35 @@
 # typed: true
 
 module Lox
+  module Visitor
+    extend T::Sig
+    extend T::Generic
+    abstract!
+
+    R = type_member
+
+    sig { abstract.params(expr: Binary).returns(R) }
+    def visit_BinaryExpr(expr); end
+
+    sig { abstract.params(expr: Grouping).returns(R) }
+    def visit_GroupingExpr(expr); end
+
+    sig { abstract.params(expr: Literal).returns(R) }
+    def visit_LiteralExpr(expr); end
+
+    sig { abstract.params(expr: Unary).returns(R) }
+    def visit_UnaryExpr(expr); end
+  end
+
   class Expr
     extend T::Sig
     extend T::Helpers
 
     abstract!
+    sig { abstract.type_parameters(:R).params(visitor: Visitor[T.type_parameter(:R)]).returns(T.type_parameter(:R))}
+    def accept(visitor); end
   end
-  
+
   class Binary < Expr
     extend T::Sig
 
@@ -27,5 +49,69 @@ module Lox
       @operator = operator
       @right = right
     end
+
+    sig { override.type_parameters(:R).params(visitor: Visitor[T.type_parameter(:R)]).returns(T.type_parameter(:R))}
+    def accept(visitor)
+      visitor.visit_BinaryExpr(self)
+    end
   end
+
+
+  class Grouping < Expr
+    extend T::Sig
+
+    sig { returns(Lox::Expr) }
+    attr_reader :expression
+
+    sig { params(expression: Lox::Expr).void }
+    def initialize(expression)
+      @expression = expression
+    end
+
+    sig { override.type_parameters(:R).params(visitor: Visitor[T.type_parameter(:R)]).returns(T.type_parameter(:R))}
+    def accept(visitor)
+      visitor.visit_GroupingExpr(self)
+    end
+  end
+
+
+  class Literal < Expr
+    extend T::Sig
+
+    sig { returns(T.untyped) }
+    attr_reader :value
+
+    sig { params(value: T.untyped).void }
+    def initialize(value)
+      @value = value
+    end
+
+    sig { override.type_parameters(:R).params(visitor: Visitor[T.type_parameter(:R)]).returns(T.type_parameter(:R))}
+    def accept(visitor)
+      visitor.visit_LiteralExpr(self)
+    end
+  end
+
+
+  class Unary < Expr
+    extend T::Sig
+
+    sig { returns(Lox::Token) }
+    attr_reader :operator
+
+    sig { returns(Lox::Expr) }
+    attr_reader :right
+
+    sig { params(operator: Lox::Token,right: Lox::Expr).void }
+    def initialize(operator,right)
+      @operator = operator
+      @right = right
+    end
+
+    sig { override.type_parameters(:R).params(visitor: Visitor[T.type_parameter(:R)]).returns(T.type_parameter(:R))}
+    def accept(visitor)
+      visitor.visit_UnaryExpr(self)
+    end
+  end
+
 end
--- ch5/tools/ast.rb
+++ ch5/tools/ast.rb
@@ -12,7 +12,7 @@ module Tools
   module AST
     extend T::Sig
 
-    ASTFields = T.type_alias { T::Hash[Symbol, T.class_of(Object)] }
+    ASTFields = T.type_alias { T::Hash[Symbol, String] }
     Directory = T.type_alias { T.any(String, Pathname) }
 
     class ASTNode < T::Struct
@@ -36,9 +36,19 @@ module Tools
     def self.generate(directory)
       define_AST(directory, "Expr", {
         Binary: {
-          left: Lox::Expr,
-          operator: Lox::Token,
-          right: Lox::Expr
+          left: "Lox::Expr",
+          operator: "Lox::Token",
+          right: "Lox::Expr"
+        },
+        Grouping: {
+          expression: "Lox::Expr"
+        },
+        Literal: {
+          value: "T.untyped"
+        },
+        Unary: {
+          operator: "Lox::Token",
+          right: "Lox::Expr"
         }
       })
     end
@@ -52,34 +62,79 @@ module Tools
     }
     def self.define_AST(directory, base_name, types)
       File.open(directory, "w+") do |f|
-        f.write(%Q{
+        f.write(
+          %Q{
 # typed: true
 
 module Lox
-  class Expr
+#{self.define_visitor(base_name, types)}
+  class #{base_name}
     extend T::Sig
     extend T::Helpers
 
     abstract!
+    sig { abstract.type_parameters(:R).params(visitor: Visitor[T.type_parameter(:R)]).returns(T.type_parameter(:R))}
+    def accept(visitor); end
   end
-  #{types.map{|type, fields| self.define_type(type, fields) }.join("\n")}
-end})
+  #{types.map{|type, fields| self.define_type(base_name, type, fields) }.join("\n")}
+end}
+        )
       end
     end
 
-  sig { params(type: Symbol, fields: ASTFields).returns(String) }
-  def self.define_type(type, fields)
-    %Q{
-  class #{type} < Expr
+    sig { params(base_name: String, types: T::Hash[Symbol, ASTFields]).returns(String) }
+    def self.define_visitor(base_name, types)
+      clean(%Q{
+  module Visitor
+    extend T::Sig
+    extend T::Generic
+    abstract!
+
+    R = type_member
+
+#{types.each_pair.map{|name, _| ["    sig { abstract.params(expr: #{name}).returns(R) }\n", "    def visit_#{name}#{base_name}(expr); end"].join}.join("\n\n")}
+  end
+      })
+    end
+
+    sig { params(base_name: String, type: Symbol, fields: ASTFields).returns(String) }
+    def self.define_type(base_name, type, fields)
+      %Q{
+  class #{type} < #{base_name}
     extend T::Sig
 
-#{fields.each_pair.map{|name, t| ['    ', "sig { returns(#{t}) }\n", '    ', 'attr_reader :',name].join}.join("\n\n")}
+#{fields.each_pair.map{|name, t| ["    sig { returns(#{t}) }\n", '    attr_reader :',name].join}.join("\n\n")}
 
     sig { params(#{fields.map{|name, t| [name, ':', ' ', t].join }.join(",")}).void }
     def initialize(#{fields.map{|name, _| name}.join(",")})
-#{fields.each_pair.map{|name, _| ['      ', '@',name,' = ',name].join}.join("\n")}
+#{fields.each_pair.map{|name, _| ['      @',name,' = ',name].join}.join("\n")}
     end
-  end}
+
+    sig { override.type_parameters(:R).params(visitor: Visitor[T.type_parameter(:R)]).returns(T.type_parameter(:R))}
+    def accept(visitor)
+      visitor.visit_#{type}#{base_name}(self)
+    end
+  end
+      }
+    end
+
+    ##
+    # Transforms any string into its snakecased equivalent,
+    # ignoring the first capitalized letter, if present.
+    # Ex.:
+    #   firstName => first_name
+    #   ClassName => class_name
+    sig { params(value: String).returns(String) }
+    def self.snake_case(value)
+      value.gsub(/[A-Z]/) {|c| "_#{c.downcase}"}.sub(/\A_/,'')
+    end
+
+    ##
+    # Removes newlines
+    #
+    sig { params(value: String).returns(String) }
+    def self.clean(value)
+      value.gsub(/\A\n+/, "")
     end
   end
 end
