9b0692b38d18f8134fa94e5673ef2758e70a0f47
Thiago Brandão <194487+thiagobrandam@users.noreply.github.com>
2022-10-12 09:47:37 -0300

    Create chapter 8 folder

---
+++ ch8/lib/lox/ast_printer.rb
@@ -0,0 +1,68 @@
+# typed: true
+
+module Lox
+  class ASTPrinter
+    extend T::Sig
+    extend T::Generic
+    include Visitor
+
+    R = type_member {{ fixed: String }}
+
+    sig { params(expr: T.nilable(Expr)).returns(String) }
+    def print(expr)
+      if expr
+        expr.accept(self)
+      else
+        "-- empty --"
+      end
+    end
+
+    sig { override.params(expr: Binary).returns(String).checked(:never) }
+    def visit_BinaryExpr(expr)
+      if expr.operator.lexeme
+        parenthesize(T.must(expr.operator.lexeme), expr.left, expr.right)
+      else
+        ""
+      end
+    end
+
+    sig { override.params(expr: Grouping).returns(String).checked(:never) }
+    def visit_GroupingExpr(expr)
+      parenthesize("group", expr.expression)
+    end
+
+    sig { override.params(expr: Literal).returns(String).checked(:never) }
+    def visit_LiteralExpr(expr)
+      return "nil" if expr.value.nil?
+
+      expr.value.to_s
+    end
+
+    sig { override.params(expr: Unary).returns(String).checked(:never) }
+    def visit_UnaryExpr(expr)
+      if expr.operator.lexeme
+        parenthesize(T.must(expr.operator.lexeme), expr.right)
+      else
+        ""
+      end
+    end
+
+    sig { override.params(expr: Ternary).returns(String).checked(:never) }
+    def visit_TernaryExpr(expr)
+      parenthesize("?:", expr.clause, expr.left, expr.right)
+    end
+
+    sig { params(name: String, exprs: Expr).returns(String).checked(:never) }
+    def parenthesize(name, *exprs)
+      string = "( #{name}"
+      exprs.each do |expr|
+        string += " "
+
+        string += expr.accept(self)
+      end
+
+      string += " )"
+      string
+    end
+  end
+end
---
+++ ch8/lib/lox/interpreter.rb
@@ -0,0 +1,177 @@
+# typed: true
+
+module Lox
+  ##
+  # Interpreter for the Lox language
+  #
+  # Lox type	     | Ruby representation
+  # ---------------+--------------------
+  # Any Lox value	 | Object
+  # nil	           | nil
+  # Boolean	       | Boolean
+  # number	       | Float
+  # string	       | String
+  #
+  class Interpreter
+    extend T::Sig
+    extend T::Generic
+    include Visitor
+
+    R = type_member {{ fixed: Object }}
+
+    sig { params(expr: T.nilable(Expr)).returns(Object) }
+    def interpret(expr)
+      if expr
+        value = evaluate(expr)
+        puts stringify(value)
+      else
+        nil
+      end
+    rescue RuntimeError => e
+      Lox.runtime_error(e)
+    end
+
+    sig { override.params(expr: Grouping).returns(Object).checked(:never) }
+    def visit_GroupingExpr(expr)
+      evaluate(expr.expression)
+    end
+
+    sig { override.params(expr: Literal).returns(Object).checked(:never) }
+    def visit_LiteralExpr(expr)
+      expr.value
+    end
+
+    sig { override.params(expr: Unary).returns(Object).checked(:never) }
+    def visit_UnaryExpr(expr)
+      # You can start to see how evaluation recursively traverses the tree.
+      # We can’t evaluate the unary operator itself until after we evaluate its operand subexpression.
+      # That means our interpreter is doing a post-order traversal—each node evaluates its children before doing its own work.
+      right = evaluate(expr.right)
+      type = expr.operator.type
+
+      case type
+      when :MINUS
+        check_number_operand(expr.operator, right);
+        return (-1) * T.cast(right, Float)
+      when :BANG
+        return !truthy?(right)
+      end
+    end
+
+    sig { override.params(expr: Binary).returns(Object).checked(:never) }
+    def visit_BinaryExpr(expr)
+      left = evaluate(expr.left)
+      right = evaluate(expr.right)
+      type = expr.operator.type
+
+      case type
+      when :MINUS
+        check_number_operands(expr.operator, left, right)
+        return T.cast(left, Float) - T.cast(right, Float)
+      when :SLASH
+        check_number_operands(expr.operator, left, right)
+        return T.cast(left, Float) / T.cast(right, Float)
+      when :STAR
+        check_number_operands(expr.operator, left, right)
+        return T.cast(left, Float) * T.cast(right, Float)
+      when :PLUS
+        case [ left, right ]
+        in [ String, String ]
+          return T.cast(left, String) + T.cast(right, String)
+        in [ Float, Float ]
+          return T.cast(left, Float) + T.cast(right, Float)
+        else
+          raise RuntimeError.new(expr.operator, "Operands must be two numbers or two strings.")
+        end
+      when :GREATER
+        check_number_operands(expr.operator, left, right)
+        return T.cast(left, Float) > T.cast(right, Float)
+      when :GREATER_EQUAL
+        check_number_operands(expr.operator, left, right)
+        return T.cast(left, Float) >= T.cast(right, Float)
+      when :LESS
+        check_number_operands(expr.operator, left, right)
+        return T.cast(left, Float) < T.cast(right, Float)
+      when :LESS_EQUAL
+        check_number_operands(expr.operator, left, right)
+        return T.cast(left, Float) <= T.cast(right, Float)
+      when :BANG_EQUAL
+        return !equal?(left, right)
+      when :EQUAL
+        return equal?(left, right)
+      end
+    end
+
+    sig { override.params(expr: Ternary).returns(Object).checked(:never) }
+    def visit_TernaryExpr(expr)
+    end
+
+    sig { params(expr: Expr).returns(Object) }
+    def evaluate(expr)
+      expr.accept(self)
+    end
+
+    ##
+    # Lox follows Ruby’s simple rule: false and nil are falsey, and everything else is truthy.
+    #
+    sig { params(value: Object).returns(T::Boolean) }
+    def truthy?(value)
+      return false if value.nil?
+      return false if value == false
+
+      true
+    end
+
+    ##
+    # Checks for value equality
+    # Use Ruby's == method to check for that
+    sig { params(val: Object, other_val: Object).returns(T::Boolean) }
+    def equal?(val, other_val)
+      return true if val.nil? && other_val.nil?
+
+      val == other_val
+    end
+
+    ##
+    # Check if operand is a number
+    #
+    sig { params(operator: Lox::Token, value: Object).void }
+    def check_number_operand(operator, value)
+      return if value.is_a?(Float)
+
+      raise RuntimeError.new(operator, "Operand must be a number.")
+    end
+
+    ##
+    # Check if operands are both numbers
+    #
+    sig { params(operator: Lox::Token, left: Object, right: Object).void }
+    def check_number_operands(operator, left, right)
+      return if left.is_a?(Float) && right.is_a?(Float)
+
+      raise RuntimeError.new(operator, "Operands must be numbers.")
+    end
+
+    ##
+    # Returns a string representation of any value
+    # produced by the interpreter's evaluation of an expression
+    #
+    sig { params(value: Object).returns(String) }
+    def stringify(value)
+      return "nil" if value.nil?
+
+      if value.is_a?(Float)
+        text = value.to_s
+
+        # 2.0 => 2
+        if text.end_with?(".0")
+          text = T.must(text[0...-2])
+        end
+
+        return text
+      end
+
+      value.to_s
+    end
+  end
+end
